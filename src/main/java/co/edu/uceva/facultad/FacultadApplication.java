87f0eb844a6de3443761390fd4ad6c12cfdc15b5
marh0819 <miguel.rivera01@uceva.edu.co>
2023-05-03 10:20:48 -0500

    Initial commit

---
+++ src/main/java/co/edu/uceva/facultad/FacultadApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.facultad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacultadApplication.class, args);
+    }
+
+}
