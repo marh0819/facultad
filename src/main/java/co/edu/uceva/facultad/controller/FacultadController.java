c1ccc59a395ab23844bd1dee8a0cdea8cc96957d
marh0819 <miguel.rivera01@uceva.edu.co>
2023-05-03 11:29:24 -0500

    Se creo un metodo para listar las facultades

--- src/main/java/co/edu/uceva/facultad/controller/FacultadController.java
+++ src/main/java/co/edu/uceva/facultad/controller/FacultadController.java
@@ -1,13 +1,23 @@
 package co.edu.uceva.facultad.controller;
 
+import co.edu.uceva.facultad.model.entities.Facultad;
+import co.edu.uceva.facultad.model.service.IFacultadService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("facultad_service")
 public class FacultadController {
+
+    @Autowired
+    IFacultadService facultadService;
+
     /**
      * EndPoint para recibir un saludo
      * @param nombre Es el nombre del usuario
@@ -16,5 +26,17 @@ public class FacultadController {
     @GetMapping("/facultad/{nombre}")
     public String imprimir(@PathVariable("nombre") String nombre){
         return "hola " + nombre;
+    }
+
+    @GetMapping("/facultades")
+    public List<Facultad> listar(){
+
+        List <Facultad> facultades = new ArrayList<>();
+        Facultad facultad1 = new Facultad(1L, "Facultad de ingenieria");
+        Facultad facultad2 = new Facultad(2L, "Facultad de ciencias de la salud");
+        facultades.add(facultad1);
+        facultades.add(facultad2);
+
+        return facultades;
     }
 }
